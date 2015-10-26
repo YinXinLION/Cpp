8f376c4ef4ec2a08d3475aad249a8fbce0f5cb35
YinXinLION <yinxin19950816@gmail.com>
2015-10-26 17:30:37 +0800

    打印两个数的乘积

---
+++ chapter1/1.4.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 1.4.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2015年10月24日 星期六 10时18分13秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    int v1, v2;
+    std::cin >> v1 >> v2;
+    std::cout << "v1 * v2 = " << v1 * v2 << std::endl;
+    return 0;
+}
