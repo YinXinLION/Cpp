4ed0b7f473eafbdcca2128e1904a9fba9e71efb5
YinXinLION <yinxin19950816@gmail.com>
2015-10-26 17:31:23 +0800

    是否合法问题

---
+++ chapter1/1.6.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 1.6.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2015年10月24日 星期六 10时20分40秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    int v1 = 3;
+    int v2 = 6;
+    std::cout << "The sum of " << v1
+    << " and " << v2
+    << " is " << v1 + v2 << std::endl;
+    return 0;
+}
