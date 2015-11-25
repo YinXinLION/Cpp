40d2321850ec3fe355e4ed591a9ff89ff62150ec
YinXinLION <yinxin19950816@gmail.com>
2015-11-25 15:43:32 +0800

    *p不能=0 *p=nullptr

---
+++ chapter2/2.32.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 2.32.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2015年11月25日 星期三 15时17分46秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+   // int null = 0 ,*p = null;//illegal
+    int null = 0 ,*p = nullptr;
+
+    //g++ 2.32.cpp -std=c++11
+}
