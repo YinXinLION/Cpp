37fbd5fa25074e179e4ca7a9d5a2e4c054ebe897
YinXinLION <yinxin19950816@gmail.com>
2015-11-25 16:39:07 +0800

    错误信息:error:expected ';' after struct definition }

---
+++ chapter2/2.39.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 2.39.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2015年11月25日 星期三 16时36分06秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+struct Foo
+{
+    /*此处为空*/
+}
+//注意:没有分号
+int main(void)
+{
+  return 0;
+}
