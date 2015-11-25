94ab9bfe749eb1a4d257a4b2c1bfb463475d1ec2
YinXinLION <yinxin19950816@gmail.com>
2015-11-25 17:47:37 +0800

    读写string对象

---
+++ chapter3/1.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2015年11月25日 星期三 17时45分47秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void)
+{
+    string s;//空字符串
+    cin >> s;//将string对象读入s　，遇到空白停止
+    cout << s << endl; //输出s
+    return 0;
+}
