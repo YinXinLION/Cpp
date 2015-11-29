5ef4a366c64391de248765cbc96a9eb0a1995abb
YinXinLION <yinxin19950816@gmail.com>
2015-11-29 20:46:04 +0800

    不合法

---
+++ chapter3/3.9.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 3.9.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2015年11月29日 星期日 20时42分03秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void)
+{
+    string s;
+    cout << s[0] << endl;
+    //代码被解除引用和打印存储在s的第一项.由于s是空的,这样的操作是无效的
+}
