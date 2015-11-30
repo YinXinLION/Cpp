d5ab041e356e3b41d8b90e0d658d5a87a7a6d9e3
YinXinLION <yinxin19950816@gmail.com>
2015-11-30 17:56:27 +0800

    将整数存入一个vector对象

---
+++ chapter3/3.14.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: 3.14.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2015年11月30日 星期一 17时52分57秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(void)
+{
+    vector<int> v1;
+    int i ,t;
+    while(i < 3)
+    {
+        cin >> t;
+        v1.push_back(t);
+    }
+}
