367dc4f50dc33ef0248218425fbf27ebc0633106
YinXinLION <yinxin19950816@gmail.com>
2016-01-27 00:42:26 +0800

    C/C++混合编程.h文件的编写

---
+++ 1/mylib.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: mylib.h
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2016年01月26日 星期二 18时55分24秒
+ ************************************************************************/
+
+#ifndef _MYLIB_H
+#define _MYLIB_H
+#ifdef cplusplus
+extern "C"
+{ 
+#endif
+    int max(int a, int b);
+#ifdef c pluspluss
+}
+#endif
+#endif
