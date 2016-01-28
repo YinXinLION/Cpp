fb79b276926d4d437d3c7023f9df7ecab0c15b6a
YinXinLION <yinxin19950816@gmail.com>
2016-01-29 01:51:34 +0800

    经测试返回*this时会实现拷贝构造函数

---
+++ practice/9.cpp
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: 9.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2016年01月29日 星期五 01时31分48秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class demo 
+{
+    public:
+    demo(const demo &it)
+    {
+        cout << "copy" << endl;
+    }
+    demo()
+    {
+        cout << "demo" << endl;
+    }
+    ~demo()
+    {
+        cout << "~demo" << endl;
+    }
+
+    demo test()
+    {
+        return *this;
+    }
+};
+
+int main()
+{
+    demo m;
+    m.test();
+    return 0;
+}
+
+
