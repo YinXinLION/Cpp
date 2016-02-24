069bec92bb4c130af575b83602f5311bd80bf9a4
YinXinLION <yinxin19950816@gmail.com>
2016-02-24 17:26:55 +0800

    staic_cast<>()初次使用

---
+++ practice/14.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: 14.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2016年02月24日 星期三 17时17分42秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdlib.h>
+using namespace std;
+
+int main(void)
+{
+    printf("%d\n",10.11);
+    printf("%f\n",10);
+    printf("%d\n",static_cast<int>(10.11));
+    printf("%f\n",static_cast<float>(10));
+}
