2c1c023c3cee3d723d15a47b15bba18adcbd8d0f
YinXinLION <yinxin19950816@gmail.com>
2016-02-25 11:14:31 +0800

    string 经过包装后的

---
+++ practice/18.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: 18.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2016年02月25日 星期四 11时04分34秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<array>
+using namespace std;
+
+int main(void)
+{
+    string str1 = "hello ";
+    string str2 = "world!";
+    string str3 = str1 + str2;
+    cout << str3 << endl;
+}
