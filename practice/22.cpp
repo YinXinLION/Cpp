02629b665f65386625671d3a8ce36e2072599566
YinXinLION <yinxin19950816@gmail.com>
2016-02-27 20:01:19 +0800

    CPP高技术组

---
+++ practice/22.cpp
@@ -0,0 +1,37 @@
+/*************************************************************************
+	> File Name: 22.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2016年02月27日 星期六 19时35分43秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+
+int main(void)
+{
+    vector<int> myvector(3, 2);
+    myvector.push_back(11);
+    myvector.pop_back();//弹出最后一个元素
+    myvector.push_back(22);
+    myvector.insert(myvector.begin() + 2 , 333);
+    for(int i = 0; i < myvector.size(); i++)
+    {
+        cout << myvector[i] << endl;
+    }
+    myvector.erase(myvector.begin() + 2 );
+    cout << endl;
+    for(int i = 0; i < myvector.size(); i++)
+    {
+        cout << myvector[i] << endl;
+    }
+
+    myvector.clear();//删除所有元素
+    for(int i = 0; i < myvector.size(); i++)
+    {
+        cout << myvector[i] << endl;
+    }
+}
