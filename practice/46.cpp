3e2de41e4c54b9f2fddb16baec192325262568e2
YinXinLION <yinxin19950816@gmail.com>
2016-03-08 23:27:55 +0800

    const 和 引用 要在构造函数参数列表初始化

--- practice/46.cpp
+++ practice/46.cpp
@@ -28,8 +28,14 @@ class myclass
 
     myclass(int data1, int data2):coint(data1),myint(data2)
     {
+        const int *p = &coint;
+        int *q = const_cast<int *>(p);
+        *q = 100;
 
+        cout << coint << endl;
     }
+    //类的外部，一开始必须初始化，用常量强行替换，不读内存
+    //类的内部，必须构建一个类才能初始化化，const与C语言是一样的，读内存
 
     void run()
     {
@@ -46,6 +52,12 @@ class myclass
 //vs2013中cout <<< my1.myint << endl;会打印出地址， 
 int myclass::shu = 0;//初始化
 
+//类中的普通成员变量
+//类名 变量名 栈上
+//new 堆上
+// 静态成员 静态去
+// 成员函数，静态函数 代码区
+
 int main(void)
 {
     myclass my1(2, 4);
@@ -53,4 +65,6 @@ int main(void)
     int data = 30;
     my1.myint = data;//引用竟然可以改变了
     cout << my1.myint << endl;
+    void (myclass::*p)() = &myclass::run;//代码共享,所有的类对象共享对象
+    void (*q)() = &myclass::go;//静态函数，与对象没关系
 }
