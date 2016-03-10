93311cd992831a7487eaa3bb8e2477c441bea3ab
YinXinLION <yinxin19950816@gmail.com>
2016-03-10 20:17:29 +0800

    list<int>和vector<int>比较

---
+++ chapter9/9.16.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: 9.16.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2016年03月10日 星期四 20时07分45秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<list>
+using namespace std;
+
+int main(void)
+{
+    list<int> li{1,2,3,4,5};
+    vector<int> vec2{1,2,3,4,5};
+    vector<int> vec3{1,2,3,4};
+
+    cout << std::vector<int>(li.begin(),li.end() == vec2 ? "true" : "false") << endl;
+}
