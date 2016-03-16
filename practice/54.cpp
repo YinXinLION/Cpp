9a7e276bd354a556d95517991142ff2468d1ed38
YinXinLION <yinxin19950816@gmail.com>
2016-03-16 20:35:56 +0800

    text

---
+++ practice/54.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 54.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2016年03月16日 星期三 20时14分56秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(void)
+{
+    int a[5][5] = {0};
+    int (*p)[5]      = a;
+    int *b[5];
+    int **q = b;
+}
