fd6e813da68d3c9f17989fd5b9304a931ff4babe
YinXinLION <yinxin19950816@gmail.com>
2016-03-24 19:28:43 +0800

    类型转换

---
+++ chapter4/4.35.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: 4.35.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2016年03月24日 星期四 19时06分30秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(void)
+{
+    char cval;
+    int ival;
+    unsigned int ui;
+    float fval;
+    double dval;
+
+    cval = 'a' + 3;
+    fval = ui - ival * 10;
+    dval = ui * fval;
+    cval = ival + fval+ dval;
+}
