e26f8922d80d251816e513b7b11bda207019cead
YinXinLION <yinxin19950816@gmail.com>
2016-03-24 18:47:47 +0800

    表达式确定一个整数是奇数还是偶数

---
+++ chapter4/4.6.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 4.6.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2016年03月24日 星期四 18时39分06秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(void)
+{
+    int i;
+    cin>>i;
+    int t = i & 0x1;
+    cout << t << endl;
+}
