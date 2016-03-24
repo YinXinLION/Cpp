35c062aaa096c42676955d234ddf8213f847807e
YinXinLION <yinxin19950816@gmail.com>
2016-03-24 18:56:12 +0800

    a>b && b>c && c>d

---
+++ chapter4/4.11.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: 4.11.cpp
+	> Author:YinXin 
+	> Mail:yinxin19950816@gmail.com 
+	> Created Time: 2016年03月24日 星期四 18时48分22秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(void)
+{
+    int a = 4, b = 3, c = 2, d = 1;
+
+    if(a>b && b>c && c>d)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
